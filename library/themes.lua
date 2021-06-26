e719d91c96b0a588e0f931f6c0388e623c925579
print("\226\153\165") <86081255+GoodBoy08@users.noreply.github.com>
2021-06-26 13:11:49 -0700

    Create themes.lua

---
+++ library/themes.lua
@@ -0,0 +1,20 @@
+theme = {
+	fontsize = 15,
+	font = Enum.Font.Code,
+	background = "rbxassetid://6880496154",
+	backgroundcolor = Color3.fromRGB(20, 20, 20),
+	tabstextcolor = Color3.fromRGB(230, 230, 230),
+	bordercolor = Color3.fromRGB(60, 60, 60),
+	accentcolor = Color3.fromRGB(255, 189, 252),
+	accentcolor2 = Color3.fromRGB(203, 67, 0),
+	outlinecolor = Color3.fromRGB(60, 60, 60),
+	outlinecolor2 = Color3.fromRGB(0, 0, 0),
+	sectorcolor = Color3.fromRGB(30, 30, 30),
+	toptextcolor = Color3.fromRGB(255, 255, 255),
+	topcolor = Color3.fromRGB(30, 30, 30),
+	topcolor2 = Color3.fromRGB(12, 12, 12),
+	buttoncolor = Color3.fromRGB(49, 49, 49),
+	buttoncolor2 = Color3.fromRGB(39, 39, 39),
+	itemscolor = Color3.fromRGB(200, 200, 200)
+}
+return theme
